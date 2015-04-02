75df4c4de678fe5118f21c8b5f3bb229e6af2d7a
Scato Eggen <scato.eggen@gmail.com>
2015-04-02 07:31:41 +0200

    Add legacy example

---
+++ code/import.php
@@ -0,0 +1,40 @@
+<?php
+
+error_reporting(E_ALL & ~E_DEPRECATED);
+
+// open database connection
+$link = mysql_connect('localhost', 'solid', 'R3f@ct0r!ng');
+mysql_select_db('solid_refactoring');
+
+// open file
+$fh = fopen('users.csv', 'r');
+$data = fgetcsv($fh, 1000, ';');
+
+// check header
+$header = array('username', 'password', 'group');
+if ($data != $header) {
+    die("File does not contain the right headers\n");
+}
+
+// import all the things!
+while ($data = fgetcsv($fh, 1000, ';')) {
+    $result = mysql_query("SELECT * FROM groups WHERE name = '{$data[2]}'");
+    $group = mysql_fetch_assoc($result);
+
+    if (!$group) {
+        mysql_query("INSERT INTO groups (name) VALUES ('{$data[2]}')");
+        $group = array('id' => mysql_insert_id(), 'name' => $data[2]);
+    }
+
+    $result = mysql_query("SELECT * FROM users WHERE username = '{$data[0]}'");
+    $user = mysql_fetch_assoc($result);
+
+    if (!$user) {
+        mysql_query("INSERT INTO users (username, password, group_id) VALUES ('{$data[0]}', MD5('{$data[1]}'), {$group['id']})");
+    } else {
+        mysql_query("UPDATE users SET password = MD5('{$data[1]}'), group_id = {$group['id']} WHERE username = '{$data[0]}'");
+    }
+}
+
+mysql_close();
+
