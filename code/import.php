b076a5d076743ac526d0f98d0aa6f33d33ed9eee
Scato Eggen <seggen@ibuildings.nl>
2015-04-29 08:41:21 +0200

    Parameterize constructor for UserImporter

--- code/import.php
+++ code/import.php
@@ -4,5 +4,11 @@ error_reporting(E_ALL & ~E_DEPRECATED);
 
 require_once 'vendor/autoload.php';
 
-$importer = new UserImporter();
+// open file, skip header
+$fileReader = new CsvFileReader();
+
+// open database connection
+$gateway = new MysqlGateway();
+
+$importer = new UserImporter($fileReader, $gateway);
 $importer->import();
